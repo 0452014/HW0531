9dfdea81e56f4d44af026e7c3ffaca46644dbf7b
0452014 <u0452014@nkfust.edu.tw>
2018-05-31 19:32:35 +0800

    a non-constant pointer to non-constant data

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h> 
+
+void convertToUppercase(char *sPtr);
+
+int main(void) {
+	char string[] = "HOmeWOrk TONYtony";
+	
+	printf("The string before conversion is: %s\n",string);
+	convertToUppercase(string);
+	printf("The string after conversion is: %s\n",string);
+	//return 0;
+}
+
+void convertToUppercase(char *sPtr) {
+	while (*sPtr != '\0') {
+		*sPtr = toupper(*sPtr);
+		++sPtr;
+	}
+}
